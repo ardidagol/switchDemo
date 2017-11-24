295f8fee5de526e4819c07eb2a2638024fcceefd
090807060504030201 <32662263+090807060504030201@users.noreply.github.com>
2017-11-24 22:49:29 +0700

    Switch UI
    Switch UI Demo

---
+++ SwitchUI/SwitchUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwitchUI
+//
+//  Created by M.F.T on 11/24/17.
+//  Copyright © 2017 M.F.T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
